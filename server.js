f02f88d20fe144c52e4f4c6289964e58ac9b8403
jupterorbita <john.williams@outlook.com>
2018-06-11 22:20:03 -0700

    finished Mongoose MongoDB Quotes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const flash = require('express-flash');
 app.use(flash());
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/basic_mongoose');
+mongoose.connect('mongodb://localhost/quoting_dojo');
 
 var UserSchema = new mongoose.Schema({
     name:  { type: String, required: true, minlength: 3, maxlength: 10},
@@ -34,3 +34,52 @@ mongoose.model('User', UserSchema);
 var User = mongoose.model('User');
 
 mongoose.Promise = global.Promise;
+
+// ============= INDEX =====================
+app.get('/', function(req, res) {   
+        res.render('index');
+    })
+
+
+// ============= POST /QUOTES METHOD ==========
+app.post('/quotes', function(req, res) {
+    console.log("POST DATA", req.body);
+    console.log(req.body.name);
+    console.log(req.body.quote);
+    
+    var userInstance = new User({
+        name: req.body.name, 
+        quote: req.body.quote
+    });
+
+    userInstance.save(function(err) {
+        if(err) {
+            console.log('we have an error', err);
+            for(var key in err.errors){
+                req.flash('registration', err.errors[key].message);
+            }
+            res.redirect('/');
+        } 
+        else { 
+            console.log('successfully added a user!');
+            res.redirect('/quotes');
+        }
+    })
+})
+
+
+// ============= GET /QUOTES PAGE ==============
+app.get('/quotes', function(req, res){
+    console.log('>>>> SERVER > /quotes ejs =============');
+    User.find({}, function(err, users){
+        console.log(users);
+        console.log(err);
+        if(err){
+            return res.json(err)
+        }
+        
+        // console.log(users[0].name)
+        // console.log(users[0].quote);     
+        return res.render('quotes', {users: users});
+    });
+});
